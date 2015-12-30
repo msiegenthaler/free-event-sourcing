38a7d0e1d0200ce03fd7120c5e512335702e1946
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2015-12-30 09:33:20 +0100

    Modularize CommandSyntaxes.

--- src/main/scala/slfes/CommandSyntax.scala
+++ src/main/scala/slfes/CommandSyntax.scala
@@ -1,36 +1,41 @@
 package slfes
 
+import scala.language.implicitConversions
 import cats.data.Xor
 import shapeless.ops.coproduct.Inject
 import shapeless.{HList, Poly1, Coproduct}
 import slfes.utils.CoproductConstraint
 import slfes.utils.CoproductConstraint._
 
+
 case class CommandSyntax[State, Commands <: Coproduct, Events <: Coproduct, T <: Poly1](poly: T) {
   type Result[C <: Cmd] = Xor[C#Errors, Seq[Events]]
   type IsCommand[C] = Inject[Commands, C]
-  type IsEvent[Event] = Inject[Events, Event]
 
   def on[C <: Cmd : IsCommand](f: C ⇒ State ⇒ Result[C]): poly.Case.Aux[C, State ⇒ Result[C]] =
-    poly.at(c ⇒ s ⇒ f(c)(s))
+    poly.at(f)
 
-  def liftEvent[Event](event: Event)(implicit inject: Inject[Events, Event]): Events = inject(event)
+  implicit def commandSyntaxResponse[E <: Coproduct](cmd: Cmd {type Errors = E}): CommandSyntaxResponse[E, Events] =
+    CommandSyntaxResponse(cmd)
+}
 
-  // Add utility methods on the command (usage cmd.success(..))
-  implicit class CommandResponse[E <: Coproduct](val cmd: Cmd {type Errors = E}) {
-    type Res = Result[cmd.type]
+case class CommandSyntaxResponse[E <: Coproduct, Events <: Coproduct](val cmd: Cmd {type Errors = E}) {
+  private type Res = Xor[cmd.type#Errors, Seq[Events]]
+  type IsEvent[Event] = Inject[Events, Event]
 
-    def success: Res =
-      Xor.right(Seq.empty)
-    def success[Event: IsEvent](event: Event): Res =
-      Xor.right(Seq(liftEvent(event)))
-    def success(events: Seq[Events]): Res =
-      Xor.right(events)
-    /** Usage: success(Event1() :: Event2() :: HList) */
-    def success[EventHList <: HList : <*<[Events]#λ](events: EventHList): Res =
-      Xor.right(CoproductConstraint[EventHList, Events].coproductList(events))
+  private def liftEvent[Event](event: Event)(implicit inject: Inject[Events, Event]): Events = inject(event)
 
-    def fail[Error](error: Error)(implicit inject: Inject[E, Error]): Res =
-      Xor.Left(inject(error))
-  }
+  def success: Res =
+    Xor.right(Seq.empty)
+  def success[Event: IsEvent](event: Event): Res =
+    Xor.right(Seq(liftEvent(event)))
+  def success(events: Seq[Events]): Res =
+    Xor.right(events)
+  /** Usage: success(Event1() :: Event2() :: HList) */
+  def success[EventHList <: HList : <*<[Events]#λ](events: EventHList): Res =
+    Xor.right(CoproductConstraint[EventHList, Events].coproductList(events))
+  //TODO add a tupeled syntax as per http://blog.scalac.io/2015/10/15/shapeless-and-futures.html
+
+  def fail[Error](error: Error)(implicit inject: Inject[E, Error]): Res =
+    Xor.Left(inject(error))
 }
--- src/main/scala/slfes/MonadicCommandSyntax.scala
+++ src/main/scala/slfes/MonadicCommandSyntax.scala
@@ -6,39 +6,41 @@ import cats.std.list
 import shapeless.ops.coproduct.Inject
 import shapeless.{Poly1, Coproduct}
 
+
 case class MonadicCommandSyntax[State, Commands <: Coproduct, Events <: Coproduct, T <: Poly1](poly: T) {
   type Result[C <: Cmd] = Xor[C#Errors, Seq[Events]]
   type IsCommand[C] = Inject[Commands, C]
-  type IsEvent[Event] = Inject[Events, Event]
 
-  def onM[C <: Cmd : IsCommand](f: CommandContext[C] ⇒ CommandMonad[C, Unit]): poly.Case.Aux[C, State ⇒ Result[C]] = {
-    poly.at(c ⇒ s ⇒ f(CommandContext(c, s)).run.map(_._1))
+  def onM[C <: Cmd : IsCommand](f: MonadicCommandContext[State, Events, C] ⇒ CommandMonad[C, Unit]): poly.Case.Aux[C, State ⇒ Result[C]] = {
+    poly.at(c ⇒ s ⇒ f(MonadicCommandContext(c, s)).run.map(_._1))
   }
 
-  type CommandMonad[C <: Cmd, A] = WriterT[Xor[C#Errors, ?], List[Events], A]
   implicit def listMonoid = list.listAlgebra[Events]
+  type CommandMonad[C <: Cmd, A] = WriterT[Xor[C#Errors, ?], List[Events], A]
+}
 
-  case class CommandContext[C <: Cmd](command: C, state: State) {
-    //Help the compiler a bit..
-    type M1[A] = Xor[C#Errors, A]
-    type CM[A] = WriterT[M1, List[Events], A]
-    type IsError[E] = Inject[C#Errors, E]
+case class MonadicCommandContext[State, Events <: Coproduct, C <: Cmd](command: C, state: State) {
+  //Help the compiler a bit..
+  type M1[A] = Xor[C#Errors, A]
+  type CM[A] = WriterT[M1, List[Events], A]
+  type IsError[E] = Inject[C#Errors, E]
+  type IsEvent[Event] = Inject[Events, Event]
+  private implicit def listMonoid = list.listAlgebra[Events]
 
-    // Verbs for the CommandMonad
-    /** Fail the command handling with the error. */
-    def fail[Error: IsError](error: Error): CM[Unit] = {
-      val l: M1[Unit] = Xor.left(Inject[C#Errors, Error].apply(error))
-      WriterT.valueT(l)
-    }
-    /** Emit an event (in case the command handling is successful. */
-    def emit[Event: IsEvent](event: Event): CM[Unit] = {
-      val e = Inject[Events, Event].apply(event) :: Nil
-      WriterT.tell(e)
-    }
-    /** Do nothing */
-    def noop: CM[Unit] = Monad[CM].pure(())
-
-    def failIf[Error: IsError](cond: Boolean, error: ⇒ Error): CM[Unit] =
-      if (cond) fail(error) else noop
+  // Verbs for the CommandMonad
+  /** Fail the command handling with the error. */
+  def fail[Error: IsError](error: Error): CM[Unit] = {
+    val l: M1[Unit] = Xor.left(Inject[C#Errors, Error].apply(error))
+    WriterT.valueT(l)
   }
+  /** Emit an event (in case the command handling is successful. */
+  def emit[Event: IsEvent](event: Event): CM[Unit] = {
+    val e = Inject[Events, Event].apply(event) :: Nil
+    WriterT.tell(e)
+  }
+  /** Do nothing */
+  def noop: CM[Unit] = Monad[CM].pure(())
+
+  def failIf[Error: IsError](cond: Boolean, error: ⇒ Error): CM[Unit] =
+    if (cond) fail(error) else noop
 }
