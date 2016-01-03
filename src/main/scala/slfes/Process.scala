df63c0e938921e4335f0c1be5ba07a080d77deb2
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-01-03 13:22:44 +0100

    Use case class for ProcessType.

--- src/main/scala/slfes/Process.scala
+++ src/main/scala/slfes/Process.scala
@@ -3,26 +3,7 @@ package slfes
 import cats.data.Xor
 import shapeless.ops.coproduct.Inject
 
-trait ProcessType[A <: Aggregate, Id] {
-  val name: String
-  val source: A
-  val spawn: AggregateEvt[A] ⇒ Option[Id]
-  val body: Id ⇒ ProcessBody
-}
-object ProcessType {
-  def apply[A <: Aggregate, Id](name: String, source: A)(spawn: AggregateEvt[A] ⇒ Option[Id], body: Id ⇒ ProcessBody): ProcessType[A, Id] = {
-    val na = name
-    val so = source
-    val sp = spawn
-    val bo = body
-    new ProcessType[A, Id] {
-      val name = na
-      val source = so
-      val spawn = sp
-      val body = bo
-    }
-  }
-}
+case class ProcessType[A <: Aggregate, Id](name: String, source: A, spawn: AggregateEvt[A] ⇒ Option[Id], body: Id ⇒ ProcessBody)
 
 sealed trait ProcessBodyAction[+A]
 object ProcessBodyAction {
