37211efd8dd78fa3c479c351464569f142f1f9f7
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-01-05 22:54:29 +0100

    Finished CommandForIdInList.

--- src/main/scala/slfes/BoundedContext.scala
+++ src/main/scala/slfes/BoundedContext.scala
@@ -1,13 +1,11 @@
 package slfes
 
-import shapeless.ops.coproduct.Inject
-import slfes.BoundedContext.CommandForIdInList
-
 import scala.language.higherKinds
 import cats.Monad
 import cats.free.Free
 import shapeless.LUBConstraint.<<:
-import shapeless.{Coproduct, HList, ::}
+import shapeless.{HList, ::}
+import shapeless.ops.coproduct.Inject
 import shapeless.ops.hlist.Selector
 import slfes.ProcessBodyAction.CommandResult
 
@@ -27,7 +25,8 @@ sealed trait BoundedContext {
 
   def exec[F[_] : Exec, I, C <: Cmd](to: I, command: C)
     (implicit cfi: CommandForIdInList[Aggregates, I, C]): Free[F, CommandResult[C]] = {
-    val action = BoundedContextAction.Command[BoundedContext, cfi.Aggregate, C](to, command)(cfi.aggregateSelector, ???)
+    import cfi._
+    val action = BoundedContextAction.Command[BoundedContext, cfi.Aggregate, C](to, command)
     lift[F, CommandResult[C]](action)
   }
 
@@ -35,57 +34,6 @@ sealed trait BoundedContext {
 }
 object BoundedContext {
   type BoundedContextM[BC <: BoundedContext, A] = Free[BoundedContextAction[BC, ?], A]
-
-
-  sealed trait CommandForIdInList[L <: HList, I, C <: Cmd] {
-    type Command <: Coproduct
-    type Aggregate <: slfes.Aggregate {type Id = I}
-    implicit val inject: Inject[Command, C]
-    implicit val aggregateSelector: Selector[L, Aggregate]
-    def apply(command: C): Command = inject(command)
-    def aggregate(l: L): Aggregate = aggregateSelector(l)
-  }
-  object CommandForIdInList {
-    type Aux[L <: HList, I, C <: Cmd, A <: slfes.Aggregate {type Id = I}, CP <: Coproduct] = CommandForIdInList[L, I, C] {
-      type Command = CP
-      type Aggregate = A
-    }
-    implicit def valid[L <: HList, I, C <: Cmd, CP <: Coproduct](ev: CommandsFromIdInList.Aux[L, I, CP],
-      i: Inject[CP, C]): Aux[L, I, C, ev.Aggregate, CP] = new CommandForIdInList[L, I, C] {
-      type Command = CP
-      type Aggregate = ev.Aggregate
-      val inject = i
-      val aggregateSelector = ev.selector
-    }
-  }
-
-  sealed trait CommandsFromIdInList[L <: HList, I] {
-    type Aggregate <: slfes.Aggregate {type Id = I}
-    type Commands <: Coproduct
-    implicit val selector: Selector[L, Aggregate]
-    def aggregate(l: L): Aggregate = selector(l)
-  }
-  object CommandsFromIdInList {
-    def apply[L <: HList, Id](implicit a: CommandsFromIdInList[L, Id]): CommandsFromIdInList[L, Id] = a
-    type Aux[L <: HList, I, C <: Coproduct] = CommandsFromIdInList[L, I] {type Commands = C}
-    type Aux2[L <: HList, I, C <: Coproduct, A <: Aggregate {type Id = I}] = CommandsFromIdInList[L, I] {
-      type Commands = C
-      type Aggregate = A
-    }
-    implicit def head[H <: Aggregate {type Id = I}, T <: HList, I]: Aux2[H :: T, I, H#Command, H] = new CommandsFromIdInList[H :: T, I] {
-      type Commands = H#Command
-      type Aggregate = H
-      val selector = Selector.select[H, T]
-    }
-    implicit def tail[H, T <: HList, I](implicit
-      ev: CommandsFromIdInList[T, I]): Aux2[H :: T, I, ev.Commands, ev.Aggregate] = new CommandsFromIdInList[H :: T, I] {
-      override type Aggregate = ev.Aggregate
-      override type Commands = ev.Commands
-      val selector = new Selector[H :: T, Aggregate] {
-        def apply(l: H :: T) = ev.selector(l.tail)
-      }
-    }
-  }
 }
 
 case class BoundedContextType[AS <: HList : <<:[AggregateType]#λ , PS <: HList : <<:[ProcessType]#λ](name: String, aggregates: AS, processes: PS) {
@@ -98,7 +46,7 @@ case class BoundedContextType[AS <: HList : <<:[AggregateType]#λ , PS <: HList 
 
 sealed trait BoundedContextAction[BC <: BoundedContext, +A]
 object BoundedContextAction {
-  case class Command[BC <: BoundedContext, A <: Aggregate : BC#Contains, C <: Cmd : A#IsCommand](to: A#Id,
+  case class Command[BC <: BoundedContext, A <: Aggregate : BC#Contains, C <: Cmd : Inject[A#Command, ?]](to: A#Id,
     command: C)
     extends BoundedContextAction[BC, CommandResult[C]]
 
--- src/main/scala/slfes/aggregateConversions.scala
+++ src/main/scala/slfes/aggregateConversions.scala
@@ -1,6 +1,7 @@
 package slfes
 
-import shapeless.Coproduct
+import shapeless.ops.hlist.Selector
+import shapeless.{::, HList, Coproduct}
 import shapeless.ops.coproduct.Inject
 
 /** Gets the aggregate from the type of the id. */
@@ -18,12 +19,13 @@ object AggregateFromId {
 }
 
 
-/** Same as CommandForAggregate, but on aggregate's id instead of on the aggregate itself. */
+/** Checks if the Id and the Command belong to the same aggregate. The aggragate type must be made visible in the
+  * implicit scope. */
 sealed trait CommandForId[I, C <: Cmd] {
   type Command <: Coproduct
-  type Aggregate <: slfes.AggregateInterface.AuxIC[I, Command]
+  type Aggregate <: AggregateInterface.AuxIC[I, Command]
   val inject: Inject[Aggregate#Command, C]
-  def apply(command: Aggregate#Command): Aggregate#Command = inject(command)
+  def apply(command: C): Aggregate#Command = inject(command)
 }
 object CommandForId {
   def apply[Id, C <: Cmd](implicit cfi: CommandForId[Id, C]) = cfi
@@ -41,7 +43,7 @@ object CommandForId {
 /** Gets the commands from the type of the aggregate's id. */
 trait CommandsFromId[Id] {
   type Command <: Coproduct
-  type Aggregate <: slfes.AggregateInterface.AuxIC[Id, Command]
+  type Aggregate <: AggregateInterface.AuxIC[Id, Command]
   def aggregate: Aggregate
 }
 object CommandsFromId {
@@ -55,3 +57,57 @@ object CommandsFromId {
   }
 }
 
+
+/** Checks if the Id and the Command belong to the same aggregate the is a member of the list L. */
+sealed trait CommandForIdInList[L <: HList, Id, C <: Cmd] {
+  type Aggregate <: AggregateInterface.WithId[Id]
+  implicit val inject: Inject[Aggregate#Command, C]
+  implicit val aggregateSelector: Selector[L, Aggregate]
+  def apply(command: C): Aggregate#Command = inject(command)
+  def aggregate(l: L): Aggregate = aggregateSelector(l)
+}
+object CommandForIdInList {
+  type Aux[L <: HList, I, C <: Cmd, A <: slfes.AggregateInterface.WithId[I]] = CommandForIdInList[L, I, C] {
+    type Aggregate = A
+  }
+  implicit def valid[L <: HList, I, C <: Cmd, CP <: Coproduct](ev: CommandsFromIdInList.Aux[L, I, CP],
+    i: Inject[CP, C]): Aux[L, I, C, ev.Aggregate] = new CommandForIdInList[L, I, C] {
+    type Command = CP
+    type Aggregate = ev.Aggregate
+    val inject = new Inject[Aggregate#Command, C] {
+      def apply(c: C) = ev.convert(i(c))
+    }
+    val aggregateSelector = ev.selector
+  }
+}
+/** Helper for CommandForIdInList. Extracts the command coproduct of the aggregate with the given Id-type. */
+sealed trait CommandsFromIdInList[L <: HList, Id] {
+  type Aggregate <: AggregateInterface.WithId[Id]
+  type Commands <: Coproduct
+  implicit val selector: Selector[L, Aggregate]
+  def aggregate(l: L): Aggregate = selector(l)
+  def convert(c: Commands): Aggregate#Command
+}
+object CommandsFromIdInList {
+  def apply[L <: HList, Id](implicit a: CommandsFromIdInList[L, Id]): CommandsFromIdInList[L, Id] = a
+  type Aux[L <: HList, I, C <: Coproduct] = CommandsFromIdInList[L, I] {type Commands = C}
+  type Aux2[L <: HList, I, C <: Coproduct, A <: AggregateInterface.WithId[I]] = CommandsFromIdInList[L, I] {
+    type Commands = C
+    type Aggregate = A
+  }
+  implicit def head[H <: AggregateInterface.WithId[I], T <: HList, I]: Aux2[H :: T, I, H#Command, H] = new CommandsFromIdInList[H :: T, I] {
+    type Commands = H#Command
+    type Aggregate = H
+    val selector = Selector.select[H, T]
+    def convert(c: Commands): Aggregate#Command = c
+  }
+  implicit def tail[H, T <: HList, I](implicit
+    ev: CommandsFromIdInList[T, I]): Aux2[H :: T, I, ev.Commands, ev.Aggregate] = new CommandsFromIdInList[H :: T, I] {
+    override type Aggregate = ev.Aggregate
+    override type Commands = ev.Commands
+    val selector = new Selector[H :: T, Aggregate] {
+      def apply(l: H :: T) = ev.selector(l.tail)
+    }
+    def convert(c: Commands) = ev.convert(c)
+  }
+}
