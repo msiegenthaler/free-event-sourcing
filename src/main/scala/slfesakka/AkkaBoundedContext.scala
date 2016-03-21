ebcdb49d6727fe4fe57ca094c8fb7ca530057041
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-03-21 22:47:29 +0100

    Add a NaturalTransform from BCA into Future.

--- src/main/scala/slfesakka/AkkaBoundedContext.scala
+++ src/main/scala/slfesakka/AkkaBoundedContext.scala
@@ -1,8 +1,15 @@
 package slfesakka
 
 import akka.actor.{ Actor, ActorRef, Props }
+import akka.pattern.ask
+import akka.util.Timeout
+import cats.data.Xor
+import cats.~>
 import shapeless.ops.coproduct.Inject
 import slfes.{ Cmd, _ }
+
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.Try
 
 object AkkaBoundedContext {
   case class ExecuteCommand(id: CommandId, aggregateType: String, aggregateId: Any, cmd: Cmd)
@@ -28,6 +35,28 @@ object AkkaBoundedContext {
             sender() ! CommandFailed(id, s"Unknown aggregate type ${aggregateType} in bounded context ${bc.name}")
         }
     }
+  }
+}
+
+class Xxx[BC <: BoundedContext](actor: ActorRef, bc: BC)(implicit timeout: Timeout, ec: ExecutionContext)
+    extends (BoundedContextAction[BC, ?] ~> Future) {
+  def apply[A](fa: BoundedContextAction[BC, A]) = fa match {
+    case BoundedContextAction.Command(to, cmd) ⇒
+      val id = CommandId.generate
+      val msg = AkkaBoundedContext.ExecuteCommand(id, bc.name, to, cmd)
+      actor.ask(msg).flatMap(_ match {
+        case CommandExecuted(`id`) ⇒
+          Future.successful(Xor.right(()))
+        case CommandFailed(`id`, error) ⇒
+          //not very elegant, but it can only happen if there is a severe implementation error in this package which would
+          // cause every error to fail. So make this tradeoff instead of going for the more comprehensive approach with
+          // a Typeable for Error on the Cmd.
+          Try(error.asInstanceOf[cmd.Errors])
+            .map(e ⇒ Future.successful(Xor.left(e)))
+            .getOrElse(Future.failed(new IllegalArgumentException(s"Command invalid: Error type ${error.getClass.getName} is invalid")))
+        case CommandInvalid(`id`, reason) ⇒
+          Future.failed(new IllegalArgumentException(s"Command invalid: ${reason}"))
+      })
   }
 }
 
