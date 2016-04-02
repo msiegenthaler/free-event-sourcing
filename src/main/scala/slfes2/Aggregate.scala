c29bcdd8699dbf3f4411b792406684f03a1335d6
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-04-03 00:33:26 +0200

    Try easier aggregate implementation.

--- src/main/scala/slfes2/Aggregate.scala
+++ src/main/scala/slfes2/Aggregate.scala
@@ -2,8 +2,12 @@ package slfes2
 
 import scala.language.implicitConversions
 import cats.data.Xor
-import shapeless.Coproduct
+import shapeless.ops.coproduct.Folder
+import shapeless.{ Coproduct, Generic, Typeable }
 import simulacrum.typeclass
+import slfes2.syntax.CoproductCommandHandler.HandleCommand
+import slfes2.syntax.CoproductEventApplicator.ApplyEvent
+import slfes2.syntax.{ CoproductCommandHandler, CoproductEventApplicator }
 
 trait Aggregate { self ⇒
   type Id
@@ -24,4 +28,36 @@ trait Aggregate { self ⇒
   def handleCommand[C <: Command](command: C, state: State): C#Error Xor Seq[Event]
   def applyEvent(event: Event, state: State): State
   def seed(id: Id): State
+
+  implicit def typeableId: Typeable[Id]
+  implicit def typeableEvent: Typeable[Event]
+  implicit def typeableCommand: Typeable[Command]
+}
+object AggregateImplementation {
+  def apply[State, CE <: Coproduct, CC <: Coproduct](
+    aggregate: Aggregate
+  )(
+    seed: aggregate.Id ⇒ State,
+    eventApplicator: CoproductEventApplicator[aggregate.Event, State],
+    commandHandler: CoproductCommandHandler[aggregate.Command, aggregate.Event]
+  )(implicit
+    tI: Typeable[aggregate.Id],
+    tE: Typeable[aggregate.Event],
+    tC: Typeable[aggregate.Command],
+    gE: Generic.Aux[aggregate.Event, CE],
+    fE: ApplyEvent[eventApplicator.type, CE, aggregate.Event, State],
+    gC: Generic.Aux[aggregate.Command, CC],
+    fC: HandleCommand[commandHandler.type, CC, aggregate.Command, aggregate.Command, aggregate.Event]): AggregateImplementation[aggregate.Aggregate] = {
+    val seed2 = seed
+    type S = State
+    new AggregateImplementation[aggregate.Aggregate] {
+      type State = S
+      def seed(id: Id) = seed2(id)
+      def applyEvent(event: aggregate.Event, state: State) = eventApplicator.apply(event, state)
+      def handleCommand[C <: aggregate.Command](command: C, state: State) = commandHandler.handle(command)(gC, fC)
+      implicit def typeableId = tI
+      implicit def typeableEvent = tE
+      implicit def typeableCommand = tC
+    }
+  }
 }
--- src/main/scala/slfes2/accountprocessing/impl/package.scala
+++ src/main/scala/slfes2/accountprocessing/impl/package.scala
@@ -4,8 +4,19 @@ import slfes2.{ AggregateImplementation, BoundedContextImplementation }
 
 package object impl {
   val accountProcessing = {
-    implicit val account = AccountImplementation
-    implicit val transaction = TransactionImplementation
+
+    implicit val account = AggregateImplementation(Account)(
+      seed = _ ⇒ AccountState(None, false),
+      eventApplicator = AccountApplicator,
+      commandHandler = AccountHandler
+    )
+
+    implicit val transaction = AggregateImplementation(Transaction)(
+      seed = _ ⇒ TransactionState(),
+      eventApplicator = TransactionApplicator,
+      commandHandler = TransactionHandler
+    )
+
     BoundedContextImplementation(AccountProcessing)
   }
 }
