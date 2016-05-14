4f68a6263199dbaf7ff2a7853e875a5f09ec77b8
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-05-14 22:02:48 +0200

    Fix tests for process.

--- src/test/scala/freeeventsourcing/ProcessTests.scala
+++ src/test/scala/freeeventsourcing/ProcessTests.scala
@@ -1,15 +1,14 @@
 package freeeventsourcing
 
-import java.time.Instant
-import cats.Monad
-import freeeventsourcing.accountprocessing.Account.Command.{ BlockFunds, Open }
-import freeeventsourcing.accountprocessing.Account.Error.{ InsufficientFunds, NotOpen, AlreadyOpen }
+import freeeventsourcing.EventSelector.WithEventType
+import freeeventsourcing.Process.ProcessMonad
+import freeeventsourcing.ProcessAction._
 import freeeventsourcing.accountprocessing.Account.Event.{ Closed, Opened }
 import freeeventsourcing.accountprocessing.Transaction.Event.Created
 import freeeventsourcing.accountprocessing.{ Account, AccountProcessing, Transaction }
-import freeeventsourcing.syntax.ProcessSyntax
+import freeeventsourcing.support.{ ValidAggregate, ValidSelector }
 import org.scalatest.{ FlatSpec, Matchers }
-import shapeless.{ CNil, :+: }
+import shapeless.CNil
 
 class ProcessTests extends FlatSpec with Matchers {
   object TestAggregate extends Aggregate {
@@ -31,34 +30,44 @@ class ProcessTests extends FlatSpec with Matchers {
   val selectorClosed = AggregateEventSelector(Account)(Account.Id(1))[Closed]
   val selectorCreated = AggregateEventSelector(Transaction)(Transaction.Id(1))[Created]
   val selectorMyEvent = AggregateEventSelector(TestAggregate)(TestAggregate.Id(1))[TestAggregate.Event.MyEvent]
+  type AP = AccountProcessing.type
 
-  val process = new ProcessSyntax(AccountProcessing)
-  import process._
+  def awaitEvent[S <: WithEventType: EventSelector](selector: S)(implicit ev: ValidSelector[AP, S]) =
+    AwaitEvent[AP, S](selector)
+
+  def executeAction[A <: Aggregate, Cmd <: A#Command](
+    aggregateType: A, aggregate: A#Id, command: A#Command, errorHandler: Cmd#Error ⇒ ProcessMonad[AP, Unit])(
+      implicit ev: ValidAggregate[AP, A]) = {
+    Execute[AP, A, Cmd](aggregateType, aggregate, command, errorHandler)
+  }
 
   "Process " should " allow selector for aggregate in the same bounded context" in {
-    //    "AwaitEvent[AccountProcessing.type, selectorOpened.type](selectorOpened)" should compile
-    //    "AwaitEvent[AccountProcessing.type, selectorClosed.type](selectorClosed)" should compile
-    //    "AwaitEvent[AccountProcessing.type, selectorCreated.type](selectorCreated)" should compile
+    "awaitEvent(selectorOpened)" should compile
+    "awaitEvent(selectorClosed)" should compile
+    "awaitEvent(selectorCreated)" should compile
   }
 
   "Process " should " not allow selector for aggregate not in the same context" in {
-    //    "AwaitEvent(selectorMyEvent)" shouldNot compile
+    "awaitEvent(selectorMyEvent)" shouldNot compile
   }
 
   "Process " should " allow commands of aggregates in the same context" in {
-    //    """Execute[Account.type, Open](Account, Account.Id(1), Open("Mario"), ???)""" should compile
+    """
+      import freeeventsourcing.accountprocessing.Account.Command.Open
+      executeAction[Account.type, Open](Account, Account.Id(1), Open("Mario"), ???)""" should compile
   }
 
   "Process " should " not allow commands of aggregates not in the same context" in {
-    //    """Execute[TestAggregate.type, TestAggregate.Command.MyCommand](TestAggregate, TestAggregate.Id(1), TestAggregate.Command.MyCommand("Mario"), ???)""" shouldNot compile
+    """executeAction[TestAggregate.type, TestAggregate.Command.MyCommand](
+        TestAggregate, TestAggregate.Id(1), TestAggregate.Command.MyCommand("Mario"), ???)""" shouldNot compile
   }
 
   "Process " should " allow the implementation to handle errors by the command" in {
     """
-    import cats.data.Xor
-    import freeeventsourcing.ProcessAction.Execute
-    def execHandler(exec: Execute[AccountProcessing.type, Account.type, Open], r: Open#Error Xor Unit): ProcessMonad[Unit] = {
-      r.fold(exec.errorHandler.apply, _ ⇒ noop)
-    }""" should compile
+      import cats.data.Xor
+      import freeeventsourcing.accountprocessing.Account.Command.Open
+      def execHandler(exec: Execute[AccountProcessing.type, Account.type, Open], r: Open#Error Xor Unit): ProcessMonad[AP, Unit] = {
+        r.fold(exec.errorHandler.apply, _ ⇒ ???)
+      }""" should compile
   }
 }
