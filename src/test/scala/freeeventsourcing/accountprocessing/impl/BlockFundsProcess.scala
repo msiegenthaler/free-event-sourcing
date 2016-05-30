c22f6bcbacf4d3ee4344ee995229bc3e41c3e682
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-05-30 11:33:40 +0200

    WIP: Implementation of block funds process.

---
+++ src/test/scala/freeeventsourcing/accountprocessing/impl/BlockFundsProcess.scala
@@ -0,0 +1,85 @@
+package freeeventsourcing.accountprocessing.impl
+
+import java.time.Instant
+import freeeventsourcing.accountprocessing.Account.Command.{ AnnounceDeposit, BlockFunds }
+import freeeventsourcing.accountprocessing.Account.Error.{ InsufficientFunds, NotOpen }
+import freeeventsourcing.accountprocessing.Account.Event._
+import freeeventsourcing.{ AggregateEvent, EventWithMetadata, ProcessDefinition }
+import freeeventsourcing.accountprocessing.Transaction.Command._
+import freeeventsourcing.accountprocessing.Transaction.Error.{ AlreadyCanceled, AlreadyConfirmed, DoesNotExist }
+import freeeventsourcing.accountprocessing.Transaction.Event._
+import freeeventsourcing.accountprocessing.{ Account, AccountProcessing, Transaction }
+import freeeventsourcing.eventselector.AggregateTypeEventSelector
+import freeeventsourcing.syntax.ProcessSyntax
+
+/** Blocks the funds in the debited account and confirms/aborts the tx based on the result.
+ *  Updating the accounts then happens in the separate TransactionResultProcess.
+ */
+object BlockFundsProcess {
+
+  val process = ProcessDefinition(AccountProcessing, "BlockFunds")(selector)(new Body(_).main)
+
+  private[this] def selector = AggregateTypeEventSelector(Transaction)[Created]
+
+  private[this] class Body(created: EventWithMetadata[AggregateEvent[Transaction.type, Created]]) {
+    val syntax = ProcessSyntax(AccountProcessing)
+    import syntax._
+
+    def tx = created.aggregate
+    def fromAccount = created.event.from
+    def toAccount = created.event.to
+    def amount = created.event.amount
+    val completeUntil: Instant = ??? //TODO created.metadata.time.realTime?
+
+    def main: ProcessMonad[Unit] = for {
+      //Block the money in the from account and announce it to the to account
+      _ ← on(fromAccount).execute(BlockFunds(tx, amount))(
+        //TODO add a variant that directly takes ProcessMonad[_]?
+        _.catching[InsufficientFunds](_ ⇒ waitForDebitedAccount).
+          catching[NotOpen](_ ⇒ waitForDebitedAccount)
+      )
+      _ ← on(toAccount).execute(AnnounceDeposit(tx, amount))(
+        _.catching[NotOpen](_ ⇒ waitForDepositAccount)
+      )
+
+      //Wait for confirmation of the successful blocking of the money in the from account
+      _ ← firstOf(
+        //TODO we need to be able to filter that to our tx
+        _.from(fromAccount).on[Blocked].event.
+          timeout(completeUntil)(abortTransaction)
+      )
+
+      //Confirm the transaction
+      _ ← on(tx).execute(Confirm())(
+        //TODO terminateOn syntax
+        _.catching[DoesNotExist](_ ⇒ terminate).
+          catching[AlreadyCanceled](_ ⇒ terminate)
+      )
+    } yield ()
+
+    def abortTransaction = for {
+      _ ← on(tx).execute(Cancel())(
+        //TODO add terminateOn[AlreadyConfirmed] syntax
+        _.catching[AlreadyConfirmed](_ ⇒ terminate).
+          catching[DoesNotExist](_ ⇒ terminate)
+      )
+      _ ← terminate
+    } yield ()
+
+    def waitForDebitedAccount = firstOf(
+      //TODO it does not help if we just add another retroactive subscription...
+      //TODO we need to start after a specific time, else it triggers instantly
+      //TODO maybe add a execute(ProcessMonad[_]) overload?
+      _.from(fromAccount).on[BalanceChanged].execute(_ ⇒ main).
+        from(fromAccount).on[TxAborted].execute(_ ⇒ main).
+        from(fromAccount).on[Opened].execute(_ ⇒ main).
+        timeout(completeUntil)(abortTransaction)
+    ).map(_ ⇒ ()) //TODO allow the methods to accept ProcessMonad[_]
+
+    def waitForDepositAccount: ProcessMonad[Unit] = firstOf(
+      _.from(toAccount).on[Opened].execute(_ ⇒ main).
+        timeout(completeUntil)(abortTransaction)
+    ).map(_ ⇒ ()) //TODO allow the methods to accept ProcessMonad[_]
+  }
+
+}
