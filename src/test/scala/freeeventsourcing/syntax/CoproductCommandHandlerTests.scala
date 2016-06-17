ca98355e2578dca2231f1e3776a69bafcb1fed88
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2016-06-17 22:21:15 +0200

    Validate correct command execution.

--- src/test/scala/freeeventsourcing/syntax/CoproductCommandHandlerTests.scala
+++ src/test/scala/freeeventsourcing/syntax/CoproductCommandHandlerTests.scala
@@ -9,11 +9,11 @@ class CoproductCommandHandlerTests extends FlatSpec with Matchers {
 
   sealed trait Event
   object Event {
-    case class Event1() extends Event
+    case class Event1(a: String) extends Event
   }
 
   case class ErrorOne()
-  case class ErrorTwo()
+  case class ErrorTwo(a: String)
 
   sealed trait Command extends AggregateCommand
   object Command {
@@ -32,12 +32,14 @@ class CoproductCommandHandlerTests extends FlatSpec with Matchers {
       def handle[C <: Command](command: C, state: State) = doHandle(command).apply(state)
 
       implicit val first = commandCase[FirstCommand] { cmd ⇒ s: String ⇒
-        Xor.right(Event1() :: Nil)
+        Xor.right(Event1(s) :: Nil)
       }
       implicit val second = commandCase[SecondCommand] { cmd ⇒ s: String ⇒
         Xor.right(Nil)
       }
     }
+    H(FirstCommand("mario"), "state") shouldBe Xor.right(List(Event1("state")))
+    H(SecondCommand(1), "state") shouldBe Xor.right(Nil)
   }
 
   "CoproductCommandHandler " should " allow handling commands with errors" in {
@@ -45,13 +47,15 @@ class CoproductCommandHandlerTests extends FlatSpec with Matchers {
       def handle[C <: Command](command: C, state: State) = doHandle(command).apply(state)
 
       implicit val first = commandCase[FirstCommand] { cmd ⇒ s: String ⇒
-        val e = Coproduct[FirstCommand#Error](ErrorTwo())
+        val e = Coproduct[FirstCommand#Error](ErrorTwo(s))
         Xor.left(e)
       }
       implicit val second = commandCase[SecondCommand] { cmd ⇒ s: String ⇒
         Xor.right(Nil)
       }
     }
+    H(FirstCommand("mario"), "state") shouldBe Xor.left(Coproduct[FirstCommand#Error](ErrorTwo("state")))
+    H(SecondCommand(1), "state") shouldBe Xor.right(Nil)
   }
 
   "CoproductCommandHandler " should " fail to compile if no commands are handled" in {
