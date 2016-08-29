30e0bbe5f05104a73282a7b15b34e78b4f7098e1
Mario Siegenthaler <mario.siegenthaler@linkyard.ch>
2016-08-29 23:59:43 +0200

    Fix compile errors.

--- src/main/scala/freeeventsourcing/api/port/IncomingEventPort.scala
+++ src/main/scala/freeeventsourcing/api/port/IncomingEventPort.scala
@@ -7,7 +7,7 @@ import simulacrum.typeclass
 case class IncomingEvent[+Event, Seq](pos: Seq, payload: Event)
 
 /** Port that delivers external events into the bounded context. */
-@typeclass trait IncomingEventPort[EG <: EventGroup, Seq] {
+@typeclass trait IncomingEventPort[EG <: EventGroup] {
   type Event <: EG#Event
-  def publisher(lastReceived: Seq): Publisher[IncomingEvent[Event, Seq]]
+  def publisher[Seq](lastReceived: Seq): Publisher[IncomingEvent[Event, Seq]]
 }
--- src/main/scala/freeeventsourcing/api/port/PublishedEventsPort.scala
+++ src/main/scala/freeeventsourcing/api/port/PublishedEventsPort.scala
@@ -1,10 +1,11 @@
 package freeeventsourcing.api.port
 
 import freeeventsourcing.api.EventSelector
+import freeeventsourcing.api.EventSelector.WithEventType
 import org.reactivestreams.Publisher
 
 /** Port that receives the events that happen inside the bounded context. */
 trait PublishedEventsPort {
   //TODO allow to combine multiple selectors.. but basically this is it
-  def event[E](selector: EventSelector[E]): Publisher[E]
+  def event[E <: WithEventType](selector: EventSelector[E]): Publisher[E]
 }
